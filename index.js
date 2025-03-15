fa767b8f86828f8b7e74939846458e353ee57afd
Muhammad Rashid <m.rashid212823@gmail.com>
2025-03-16 02:28:19 +0500

    good to go

--- index.js
+++ index.js
@@ -23,31 +23,33 @@ app.use(morgan("common"));
 //   })
 // );
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://client-url-shortner-two.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://client-url-shortner-two.vercel.app",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (e.g., mobile apps, curl requests)
-      if (!origin) return callback(null, true);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Allow requests with no origin (e.g., mobile apps, curl requests)
+//       if (!origin) return callback(null, true);
 
-      // Check if the origin is in the allowed list
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}.`;
-        return callback(new Error(msg), false);
-      }
+//       // Check if the origin is in the allowed list
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}.`;
+//         return callback(new Error(msg), false);
+//       }
 
-      // Allow the request
-      return callback(null, true);
-    },
-    methods: ["GET", "POST", "OPTIONS"], // Allow only GET, POST, and OPTIONS
-    allowedHeaders: ["Content-Type"], // Allow only specific headers
-    credentials: true, // Allow credentials (if needed)
-  })
-);
+//       // Allow the request
+//       return callback(null, true);
+//     },
+//     methods: ["GET", "POST", "OPTIONS"], // Allow only GET, POST, and OPTIONS
+//     allowedHeaders: ["Content-Type"], // Allow only specific headers
+//     credentials: true, // Allow credentials (if needed)
+//   })
+// );
+
+app.use(cors({ origin: "http://192.168.100.4:3000" }));
 
 // app.use(cors());
 app.use("/url", url_route);
