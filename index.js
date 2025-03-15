831bb9871db829c1950a889fb00c7c930d6a3077
Muhammad Rashid <m.rashid212823@gmail.com>
2025-03-16 00:41:44 +0500

    cors andh dhund

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ const { MONGO_URI, PORT } = process.env;
 app.use(express.json());
 app.use(morgan("common"));
 // app.options("*", cors()); // Handle preflight requests for all routes
-app.use(
-  cors({
-    origin: ["https://server-url-shortner.vercel.app", "http://localhost:3000"], // Allow requests from these origins
-    methods: ["GET", "POST"], // Allow only GET and POST requests
-    allowedHeaders: ["Content-Type"], // Allow only specific headers
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://server-url-shortner.vercel.app", "http://localhost:3000"], // Allow requests from these origins
+//     methods: ["GET", "POST"], // Allow only GET and POST requests
+//     allowedHeaders: ["Content-Type"], // Allow only specific headers
+//   })
+// );
+
+app.use(cors("*"));
 app.use("/url", url_route);
 
 mongoose
